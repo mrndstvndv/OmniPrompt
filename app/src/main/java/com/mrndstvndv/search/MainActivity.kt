2b1576c3d41fd288eac93745a8f7ae03fa99def3
mrndstvndv <stvndvmrnd@gmail.com>
2026-06-13 11:27:10 +0800

    chore: remove unused slideInVertically import

--- app/src/main/java/com/mrndstvndv/search/MainActivity.kt
+++ app/src/main/java/com/mrndstvndv/search/MainActivity.kt
@@ -21,7 +21,6 @@ import androidx.compose.animation.expandVertically
 import androidx.compose.animation.fadeIn
 import androidx.compose.animation.fadeOut
 import androidx.compose.animation.shrinkVertically
-import androidx.compose.animation.slideInVertically
 import androidx.compose.foundation.background
 import androidx.compose.foundation.clickable
 import androidx.compose.foundation.combinedClickable
